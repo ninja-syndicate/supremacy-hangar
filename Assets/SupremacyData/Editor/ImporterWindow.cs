8949514c3d9c0c44ac7eeca5cc576974807e9b9e
Brendan Ragan <lordmortis@gmail.com>
2022-06-10 10:59:22 +0800

    create data bundle location

--- Assets/SupremacyData/Editor/ImporterWindow.cs
+++ Assets/SupremacyData/Editor/ImporterWindow.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 namespace SupremacyData.Editor
 {
-    public class ImporterWindow : UnityEditor.EditorWindow
+    public class ImporterWindow : EditorWindow
     {
+        private Runtime.Data myData;
+        
         [MenuItem("Supremacy/Data/Importer")]
         public static void Spawn()
         {
-            var window = EditorWindow.GetWindow<ImporterWindow>();
+            var window = GetWindow<ImporterWindow>();
             window.Show();
         }
 
         public void OnGUI()
         {
-            GUILayout.Label("I AM WINDOW");
+            myData = EditorGUILayout.ObjectField(
+                "Static Data Object",
+                myData, 
+                typeof(Runtime.Data), 
+                false) as Runtime.Data;
+            EditorGUI.BeginDisabledGroup(myData);
+            if (GUILayout.Button("Create")) CreateDataObject();
+            {
+                Debug.Log("Create!");
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+
+        private void CreateDataObject()
+        {
+            myData = CreateInstance<Runtime.Data>();
+            AssetDatabase.CreateAsset(myData, "Assets/Settings/Static Data.asset");
+            AssetDatabase.SaveAssets();
         }
     }
 }
