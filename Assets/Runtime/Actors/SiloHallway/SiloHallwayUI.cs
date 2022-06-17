9d9734e93538c3993934b5cfa6e99adb52bc96b9
Brendan Ragan <lordmortis@gmail.com>
2022-06-17 10:25:38 +0800

    start building new proper hallway UI

--- Assets/Runtime/Actors/SiloHallway/SiloHallwayUI.cs
+++ Assets/Runtime/Actors/SiloHallway/SiloHallwayUI.cs
@@ -1,7 +1,64 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace SupremacyHangar.Runtime.Actors.SiloHallway
 {
-    public class SiloHallwayUI
+    [RequireComponent(typeof(Canvas))]
+    public class SiloHallwayUI : MonoBehaviour
     {
+        [SerializeField] private Color testFactionColorChange;
         
+        [SerializeField] private Image[] factionColorImages;
+        [SerializeField] private Button[] factionColorButtons;
+        [SerializeField] private TMP_Text[] factionColorText;
+        [SerializeField] private float darkerFactionColorMultiplier;
+        [SerializeField] private Image[] darkerFactionColorImages;
+
+        [SerializeField] private TMP_Text siloNumber;
+        [SerializeField] private TMP_Text siloContentsType;
+        [SerializeField] private TMP_Text siloContentsName1;
+        [SerializeField] private TMP_Text siloContentsName2;
+        [SerializeField] private Button loadButton;
+        
+        private Color currentFactionColor = Color.black;
+
+        public void UpdateFactionColor(Color newColor)
+        {
+            if (newColor == currentFactionColor) return;
+            currentFactionColor = newColor;
+            foreach (var image in factionColorImages)
+            {
+                image.color = newColor;
+                image.SetMaterialDirty();
+            }
+
+            foreach (var button in factionColorButtons)
+            {
+                button.image.color = newColor;
+                button.image.SetMaterialDirty();
+            }
+
+            foreach (var text in factionColorText)
+            {
+                text.color = newColor;
+                text.SetMaterialDirty();
+            }
+            foreach (var image in darkerFactionColorImages)
+            {
+                image.color = newColor * darkerFactionColorMultiplier;
+                image.SetMaterialDirty();
+            }
+        }
+        
+#if UNITY_EDITOR
+        public void Update()
+        {
+            if (currentFactionColor != testFactionColorChange)
+            {
+                UpdateFactionColor(testFactionColorChange);
+            }
+        }
+#endif        
     }
 }
