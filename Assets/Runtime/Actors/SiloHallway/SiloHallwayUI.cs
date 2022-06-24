6189a5238d586eeb9507dca6e6dab18ac50a5e6c
Brendan Ragan <lordmortis@gmail.com>
2022-06-24 15:18:10 +0800

    add Silo Offset to editor, refactor foldout code

--- Assets/Runtime/Actors/SiloHallway/SiloHallwayUI.cs
+++ Assets/Runtime/Actors/SiloHallway/SiloHallwayUI.cs
@@ -19,6 +19,8 @@ namespace SupremacyHangar.Runtime.Actors.SiloHallway
         [SerializeField] private TMP_Text siloContentsName1;
         [SerializeField] private TMP_Text siloContentsName2;
         [SerializeField] private Button loadButton;
+
+        [SerializeField] private int siloOffset;
         
         private Color currentFactionColor = Color.black;
 
