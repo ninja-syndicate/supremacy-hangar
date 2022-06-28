d603c845eb737680776ce785a08c01ecb2eef473
Peter Neve <69139852+UpwindPlains70@users.noreply.github.com>
2022-06-28 16:41:10 +0800

    Menu start
    could update to send signal out of paused instead of changing TimeScale

---
+++ Assets/Runtime/Actors/SliderValueToText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class SliderValueToText : MonoBehaviour
+{
+    [SerializeField] private Slider sliderUI;
+    [SerializeField] private TextMeshProUGUI textSliderValue;
+
+    [SerializeField] private bool isAudio = false;
+    void Start()
+    {
+        textSliderValue = GetComponent<TextMeshProUGUI>();
+        ShowSliderValue();
+    }
+
+    //Assign text field to show the value a the slider
+    public void ShowSliderValue()
+    {
+        float sliderValue = isAudio ? sliderUI.value * 100 : sliderUI.value;
+
+        //Output for whole number
+        if (sliderUI.wholeNumbers)
+            textSliderValue.text = "(" + sliderUI.value + ")";
+        else //Output for floating number (decimal point)
+            textSliderValue.text = isAudio ? "(" + sliderValue.ToString("F0") + ")" : "(" + sliderValue.ToString("F1") + ")";
+    }
+}
