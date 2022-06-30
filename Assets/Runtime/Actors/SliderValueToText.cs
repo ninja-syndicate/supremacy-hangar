48e4b7a55f5161bbcde3bc7ee2fbe420081cfc5c
Peter Neve <69139852+UpwindPlains70@users.noreply.github.com>
2022-06-30 15:02:08 +0800

    Error fix & refactor menu & volume set

--- Assets/Runtime/Actors/SliderValueToText.cs
+++ Assets/Runtime/Actors/SliderValueToText.cs
@@ -7,8 +7,6 @@ public class SliderValueToText : MonoBehaviour
 {
     [SerializeField] private Slider sliderUI;
     [SerializeField] private TextMeshProUGUI textSliderValue;
-
-    [SerializeField] private bool isAudio = false;
     void Start()
     {
         textSliderValue = GetComponent<TextMeshProUGUI>();
@@ -18,12 +16,11 @@ public class SliderValueToText : MonoBehaviour
     //Assign text field to show the value a the slider
     public void ShowSliderValue()
     {
-        float sliderValue = isAudio ? sliderUI.value * 100 : sliderUI.value;
 
         //Output for whole number
         if (sliderUI.wholeNumbers)
             textSliderValue.text = "(" + sliderUI.value + ")";
         else //Output for floating number (decimal point)
-            textSliderValue.text = isAudio ? "(" + sliderValue.ToString("F0") + ")" : "(" + sliderValue.ToString("F1") + ")";
+            textSliderValue.text = "(" + sliderUI.value.ToString("F1") + ")";
     }
 }
