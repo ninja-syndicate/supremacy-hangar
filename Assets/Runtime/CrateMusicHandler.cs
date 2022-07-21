901c934dc573aeadedaed2537d510e58d5182ccd
bsfdk2 <joshua.stephen@weareninja.com>
2022-07-21 17:56:42 +0800

    Implemented Music Cue for Item Rarity. Not linked to the actual rarity of the item that the physical crate unboxes in the silo itself so no sound is going to be played but it is ready and set up.

---
+++ Assets/Runtime/CrateMusicHandler.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace SupremacyHangar.Runtime.Silo
+{
+    [RequireComponent(typeof(AudioSource))]
+
+    public class CrateMusicHandler : MonoBehaviour
+    {
+        //private CrateDoorController crateDoorController;
+        
+        [SerializeField] private AudioClip acInherited, acCollosal, acRare, acLegendary, acExotic, acMythic;
+        private AudioSource myAudioSource;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            myAudioSource = GetComponent<AudioSource>();
+            if (myAudioSource == null) { Debug.LogError("Audio Source Broken...", this); }
+        }
+
+        public void PlayCrateMusic(int crateRarity)
+        {
+            myAudioSource.Stop();
+            
+            if (crateRarity < 0 || crateRarity > 5) { Debug.LogError("Crate Rarity Invalid. Make sure value is an integer between 0 and 5!", this); }
+
+            switch (crateRarity)
+            {
+                case 0:
+                    myAudioSource.clip = acInherited;
+                    break;
+                case 1:
+                    myAudioSource.clip = acCollosal;
+                    break;
+                case 2:
+                    myAudioSource.clip = acRare;
+                    break;
+                case 3:
+                    myAudioSource.clip = acLegendary;
+                    break;
+                case 4:
+                    myAudioSource.clip = acExotic;
+                    break;
+                case 5:
+                    myAudioSource.clip = acMythic;
+                    break;
+            }
+
+            myAudioSource.Play();
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
--- Assets/Runtime/Silo/CrateDoorController.cs
+++ Assets/Runtime/Silo/CrateDoorController.cs
@@ -5,23 +5,46 @@ namespace SupremacyHangar.Runtime.Silo
 {
     //TODO: Refactor this
     [RequireComponent(typeof(AudioSource))]
+
     public class CrateDoorController : MonoBehaviour
     {
         private SignalBus _bus;
-        
+
         private bool _subscribed;
 
+        [SerializeField] private CrateMusicHandler crateMusicHandler;
         [SerializeField] private Animator _animator;
+        private AnimatorClipInfo[] _animatorClipInfo;
         private AudioSource myAudioSource;
         [SerializeField] private AudioClip OpenCrateClip;
         [SerializeField] private AudioClip CloseCrateClip;
+
+        private bool stingerPlayedYet;
 
         [Inject]
         public void Contruct(SignalBus bus)
         {
             _bus = bus;
             SubscribeToSignal();
+        }
+
+        void Start()
+        {
             myAudioSource = gameObject.GetComponent(typeof(AudioSource)) as AudioSource;
+            stingerPlayedYet = false;
+
+            _animatorClipInfo = this._animator.GetCurrentAnimatorClipInfo(0);
+        }
+
+        void Update()
+        {
+            _animatorClipInfo = _animator.GetCurrentAnimatorClipInfo(0);
+            
+            if (_animatorClipInfo[0].clip.name == "Open" && !stingerPlayedYet)
+            {
+                //TriggerUponAnimationEnd(5);
+                stingerPlayedYet = true;
+            }
         }
 
         private void OnEnable()
@@ -41,6 +64,11 @@ namespace SupremacyHangar.Runtime.Silo
             if (_bus == null || _subscribed) return;
             _bus.Subscribe<OpenCrateSignal>(OpenCrate);
             _subscribed = true;
+        }
+
+        private void TriggerUponAnimationEnd(int temp)
+        {
+            crateMusicHandler.PlayCrateMusic(temp);
         }
 
         private void OpenCrate()
